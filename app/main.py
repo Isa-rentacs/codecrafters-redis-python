777a6ea2d9f1b5da60e56ad54902ac72a3d79601
Isa-Rentacs <kurodaju@LAPTOP-0O2SU6GG.localdomain>
2023-01-06 01:20:19 -0800

    Implement response for PING

--- app/main.py
+++ app/main.py
@@ -6,7 +6,13 @@ def main():
     print("Logs from your program will appear here!")
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, addr = server_socket.accept() # wait for client
+
+    while True:
+        msg = connection.recv(1024)
+        connection.send(b"+PONG\r\n")
+    
+
 
 
 if __name__ == "__main__":
